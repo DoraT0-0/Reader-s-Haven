38f987e6dfbb50976fa505967965bcc6b876f182
DoraT0-0 <payvin.k.01@gmail.com>
2024-11-30 13:44:28 +0300

    0.0.4

--- lib/modules/description/view/description_page.dart
+++ lib/modules/description/view/description_page.dart
@@ -1,8 +1,8 @@
 
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
-
+import 'package:http/http.dart' as http;
 
 @RoutePage()
 class DescriptionPage extends StatefulWidget {
@@ -16,31 +16,48 @@ class DescriptionPage extends StatefulWidget {
 }
 
 class _DescriptionPageState extends State<DescriptionPage> {
-  var favorite = false;
+  late List response = [];
+    Future<void> getinfoinbdid() async{
+    try{
+      String uri = "http://readershavenapi-dorat0.amvera.io/books/${widget.id}";
+      var res = await http.get(Uri.parse(uri));
+      
+      setState(() {
+        response = jsonDecode(res.body);
+      });
+     
+    }catch(e){
+      print(e);
+    }
+  }
+  @override
+  void initState(){
+    super.initState();
+    getinfoinbdid();
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
+    return response.isEmpty ? Center(child: CircularProgressIndicator(
+                  color: theme.indicatorColor,
+                  strokeWidth: 3,
+      ),
+      ): Scaffold(
       appBar: AppBar(
         backgroundColor: theme.primaryColor,
-        title: Text(widget.id.toString()),
+        title: Text(response[0]["title"]),
       ),
-      // body: Center(
-      //   child: Column(children: [
-      //     SizedBox(child: Image.asset('assets/icons/icon_app.png'), width: 100, height: 100),
-      //     SizedBox(height: 10,),
-      //     Center(child: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),)),
-      //     SizedBox(height: 5,),
-      //     Center(child: Text(widget.data, style: TextStyle(fontStyle: FontStyle.italic),)),
-      //     SizedBox(height: 5,),
-      //     SizedBox(width: 250,child: Center(child: Text(widget.description),)),
-      //     SizedBox(
-      //       width: 150,
-      //       child: 
-      //       IconButton(onPressed: () {setState(() {favorite = !favorite;});}, 
-      //       icon: favorite?Icon(Icons.favorite, color: theme.indicatorColor,) :Icon(Icons.favorite_outline, color: theme.indicatorColor,)),)
-      //   ],),
-      // ),
+      body: Column(children: [
+          SizedBox(height: 10,),
+          SizedBox(child: Image.network(response[0]["image_url"]), width: 150, height: 150),
+          SizedBox(height: 10,),
+          Center(child: Text(response[0]["title"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),)),
+          SizedBox(height: 5,),
+          Center(child: Text(response[0]["date"], style: TextStyle(fontStyle: FontStyle.italic),)),
+          SizedBox(height: 5,),
+          SizedBox(width: 250,child: Center(child: Text(response[0]["description"]),)),
+        ],),
     );
   }
 }
